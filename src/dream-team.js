a6c1afbdb22d10e21ed99395112f46693136fa45
mamaNikol <49046890+mamaNikol@users.noreply.github.com>
2020-02-17 10:35:27 +0800

    feat: add right solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,12 +1,10 @@
-module.exports = function createDreamTeam(arr) {
+module.exports = function createDreamTeam(members) {
   throw 'Not implemented';
   // remove line with error and write your code here
  if (!Array.isArray(members)){
  return false;
 }
- if (!Array.isArray(members)){
- return false;
-}
+
 let str = '';
   for(let i = 0; i < members.length; i++){
     if(typeof members[i] === 'string'){
