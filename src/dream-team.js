5dfbc6ec3821deffa40c6bc1d238e19fec5443a0
mamaNikol <49046890+mamaNikol@users.noreply.github.com>
2020-02-17 09:24:39 +0800

    feat: solve more

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam(arr) {
   throw 'Not implemented';
   // remove line with error and write your code here
+  let result = [];
+	for (let item of arr){
+		result.push(item[0]);
+	}return result.toUpperCase().sort();
+
+}
+
 };
