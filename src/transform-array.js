9966567ef7492ff3229e7b95f8df0e8b8b427704
mamaNikol <49046890+mamaNikol@users.noreply.github.com>
2020-02-18 12:50:43 +0800

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,26 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+function transform(arr ) {
+	if(!Array.isArray(arr)){
+		throw new Error();
+	}
+for(let item of arr){
+	if(arr[item] === '--discard-next'){
+		let index = arr.indexOf( '--discard-next');
+   arr.splice(index,2);
+     return arr;
+	}if(arr[item] === '--discard-prev'){
+		let index2 = arr.indexOf('--discard-prev');
+		arr.splice(index2-1,2);
+		return arr;
+	}if(arr[item] === '--double-next'){
+		let index3 = arr.indexOf('--double-next');
+		arr.splice(index3,1,index3+1);
+		return arr;
+	}if(arr[item] === '--double-prev'){
+		let index4 = arr.indexOf('--double-prev');
+		arr.splice(index4,1,index4-0);
+		return arr;
+	}
+}
+   
 };
