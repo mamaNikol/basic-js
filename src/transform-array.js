565491ad09b662b9e45d7d4bd7428ab3659ac958
mamaNikol <belief1987@ya.ru>
2020-02-20 19:40:07 +0900

    feat: change solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,26 +1,26 @@
 module.exports = function transform(arr) {
-function transform(arr ) {
-	if(!Array.isArray(arr)){
-		throw new Error();
-	}
-for(let item of arr){
-	if(arr[item] === '--discard-next'){
-		let index = arr.indexOf( '--discard-next');
-   arr.splice(index,2);
-     return arr;
-	}if(arr[item] === '--discard-prev'){
-		let index2 = arr.indexOf('--discard-prev');
-		arr.splice(index2-1,2);
-		return arr;
-	}if(arr[item] === '--double-next'){
-		let index3 = arr.indexOf('--double-next');
-		arr.splice(index3,1,index3+1);
-		return arr;
-	}if(arr[item] === '--double-prev'){
-		let index4 = arr.indexOf('--double-prev');
-		arr.splice(index4,1,index4-0);
-		return arr;
-	}
-}
-   
+  if (!Array.isArray(arr)) throw new Error('not an array');
+  if (arr.length < 1) return [];
+  
+  const transormedArr = [];
+
+  for (let i = 0, length = arr.length; i < length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i++;
+        break;
+      case '--discard-prev':
+        transormedArr.length !== 0 ? transormedArr.pop() : null;
+        break;
+      case '--double-next':
+        i < length - 1 ? transormedArr.push(arr[i + 1]) : null;
+        break;
+      case '--double-prev':
+        i >= 1 ? transormedArr.push(arr[i - 1]) : null;
+        break;
+      default:
+        transormedArr.push(arr[i]);
+    }
+  }
+  return transormedArr;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -11,12 +11,11 @@ module.exports = function getSeason(date ) {
    	winter : [11, 0, 1],
    	spring : [2,3,4],
    	summer: [5,6,7],
-   	authumn: [8,9,10]
+   	autumn: [8,9,10]
    };
    for(let season in seasonsValue){
    	if(seasonsValue[season].includes(month)){
    		return season;
    	}
    }
-};
 };
